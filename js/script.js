9e7f5f62cb0de1f459cec8f77d9f7572a083eb58
gpo993 <gpo993it@gmail.com>
2023-10-14 09:00:44 +0300

    final changes

--- js/script.js
+++ js/script.js
@@ -1,75 +1,75 @@
-const pickWord = () => {
-  const words = [
-    'c',
-    'c#',
-    'c++',
-    'python',
-    'java',
-    'javascript',
-    'python',
-    'sql',
-    'php',
-    'golang',
-    'kotlin',
-    'scala',
-    'ruby',
-    'typescript',
-    'sql',
-    'haskell',
-    'rust'
-  ];
+function guessTheWord() {
+  const pickWord = () => {
+    const words = [
+      'c',
+      'c#',
+      'c++',
+      'python',
+      'java',
+      'javascript',
+      'typescript',
+      'python',
+      'php'
+    ];
+  
+    return words[Math.floor(Math.random() * words.length)];
+  };
+  
+  const setupAnswerArray = (word) => {
+    let answerArray = [];
+    for (let i = 0; i < word.length; i++) {
+      answerArray[i] = "_";
+    }
+  
+    return answerArray;
+  };
+  
+  const showPlayerProgress = (answerArray) => {
+    alert(answerArray.join(" "));
+  };
+  
+  const getGuess = () => prompt("Guess a letter of programming language, or click Cancel to stop playing.");
+  
+  const updateGameState = (guess, word, answerArray) => {
+    let appearances = 0;
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        appearances++;
+      }
+    }
+  
+    return appearances;
+  };
+  
+  const showAnswerAndCongratulatePlayer = (answerArray) => {
+    showPlayerProgress(answerArray);
+    alert("Good job! The answer was " + answerArray.join(""));
 
-  return words[Math.floor(Math.random() * words.length)];
-};
-
-const setupAnswerArray = (word) => {
-  let answerArray = [];
-  for (let i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-  }
-
-  return answerArray;
-};
-
-const showPlayerProgress = (answerArray) => {
-  alert(answerArray.join(" "));
-};
-
-const getGuess = () => prompt("Guess a letter of programming language, or click Cancel to stop playing.");
-
-const updateGameState = (guess, word, answerArray) => {
-  let appearances = 0;
-  for (let j = 0; j < word.length; j++) {
-    if (word[j] === guess) {
-      answerArray[j] = guess;
-      appearances++;
+    document.body.style.background = 'url("./img/bg-image.png")';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+  };
+  
+  const word = pickWord();
+  const answerArray = setupAnswerArray(word);
+  let remainingLetters = word.length;
+  
+  while (remainingLetters > 0) {
+    showPlayerProgress(answerArray);
+    let guess = getGuess();
+  
+    if (guess === null) {
+      break;
+    } else if (guess.length !== 1) {
+      alert("Please enter a single letter.");
+    } else {
+      let correctGuesses = updateGameState(guess, word, answerArray);
+      remainingLetters -= correctGuesses;
     }
   }
-
-  return appearances;
-};
-
-const showAnswerAndCongratulatePlayer = (answerArray) => {
-  showPlayerProgress(answerArray);
-  alert("Good job! The answer was " + answerArray.join(""));
-};
-
-const word = pickWord();
-const answerArray = setupAnswerArray(word);
-let remainingLetters = word.length;
-
-while (remainingLetters > 0) {
-  showPlayerProgress(answerArray);
-  let guess = getGuess();
-
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
-  } else {
-    let correctGuesses = updateGameState(guess, word, answerArray);
-    remainingLetters -= correctGuesses;
-  }
+  
+  showAnswerAndCongratulatePlayer(answerArray);
 }
 
-showAnswerAndCongratulatePlayer(answerArray);
+guessTheWord();
